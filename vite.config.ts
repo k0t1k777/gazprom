c88ac45022194eba2008ebf9fe71029cd6c11517
Nickolai <lindeman072@mail.ru>
2024-07-31 17:21:08 +0400

    fix/deploy6

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/gazprom/',
   plugins: [react(), svgr({ svgrOptions: {} })],
   css: {
     preprocessorOptions: {
