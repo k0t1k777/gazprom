75e507b774c975c6e5eadee110117b67dff5d92c
Nickolai <lindeman072@mail.ru>
2024-08-01 18:29:37 +0400

    feat/ card

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,47 @@
+import styles from 'src/components/Filter/Filter.module.scss';
+import { useRef } from 'react';
+import useOutsideClick from 'src/hooks/useOutsideClick';
+import { Input } from 'antd';
+import { CloseOutlined, DownOutlined } from '@ant-design/icons';
+import Card from '../Card/Card';
+
+interface FilterProps {
+  isFilterOpen: boolean;
+  setIsFilterOpen: (type: boolean) => void;
+}
+
+export default function Filter({ setIsFilterOpen }: FilterProps) {
+  const ref = useRef(null);
+
+  useOutsideClick(ref, () => {
+    setIsFilterOpen(false);
+  });
+
+  return (
+    <div ref={ref} className={styles.filter}>
+      <Input className={styles.input} placeholder='Поиск' />
+      <div className={styles.container}>
+        <CloseOutlined className={styles.img} />
+        <p className={styles.text}>Фильтры</p>
+      </div>
+      <ul className={styles.list}>
+        <li className={styles.containerItem}>
+          <p className={styles.item}>Подразделение</p>
+          <DownOutlined className={styles.arrow} />
+        </li>
+
+        <li className={styles.containerItem}>
+          <p className={styles.item}>Должность</p>
+          <DownOutlined className={styles.arrow} />
+        </li>
+        <li className={styles.containerItem}>
+          <p className={styles.item}>Город</p>
+          <DownOutlined className={styles.arrow} />
+        </li>
+      </ul>
+      <div>
+        <Card />
+      </div>
+    </div>
+  );
+}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,17 +3,38 @@ import Logo from 'src/assets/Logo.svg?react';
 import { Input } from 'antd';
 import { FilterOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Filter from 'src/components/Filter/Filter';
+import cn from 'classnames/bind';
+const cx = cn.bind(styles);
 
 export default function Header() {
+  const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
+
   return (
-    <header className={styles.header}>
-      <Link to='/'>
-        <Logo />
-      </Link>
-      <div className={styles.container}>
-        <Input className={styles.input} placeholder='Поиск' />
-        <FilterOutlined className={styles.headerIcon} />
-      </div>
-    </header>
+    <>
+      <header className={styles.header}>
+        <Link to='/'>
+          <Logo />
+        </Link>
+        <div className={styles.container}>
+          <Input
+            placeholder='Поиск'
+            className={cx(styles.input, {
+              [styles.input_dissabled]: isFilterOpen,
+            })}
+          />
+          <FilterOutlined
+            onClick={() => setIsFilterOpen(!isFilterOpen)}
+            className={cx(styles.headerIcon, {
+              [styles.headerIcon_dissabled]: isFilterOpen,
+            })}
+          />
+        </div>
+      </header>
+      {isFilterOpen && (
+        <Filter setIsFilterOpen={setIsFilterOpen} isFilterOpen={isFilterOpen} />
+      )}
+    </>
   );
 }
